23d6693449c2dbfd302f3ee2c9f18602ba17b362
Aleksandar Enchev <aleksandar.enchev@icloud.com>
2025-10-28 19:55:22 +0200

    remove HeroUI components, configurations, and dependencies from the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import "@/styles/globals.css";
-import { Metadata, Viewport } from "next";
-import { Link } from "@heroui/link";
-import clsx from "clsx";
-
+import React from "react";
 import { Providers } from "./providers";
-
+import { Metadata, Viewport } from "next";
 import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
+import { Sansation } from "next/font/google";
 
 export const metadata: Metadata = {
   title: {
@@ -19,6 +15,12 @@ export const metadata: Metadata = {
     icon: "/favicon.ico",
   },
 };
+
+const sansation = Sansation({
+  weight: ["300", "400", "700"],
+  subsets: ["cyrillic", "latin"],
+  fallback: ["system-ui", "sans-serif"],
+});
 
 export const viewport: Viewport = {
   themeColor: [
@@ -33,32 +35,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning lang="en">
-      <head />
-      <body
-        className={clsx(
-          "min-h-screen text-foreground bg-background font-sans antialiased",
-          fontSans.variable,
-        )}
-      >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
-            </footer>
-          </div>
+    <html suppressHydrationWarning lang="bg">
+      <body className={`${sansation.className} antialiased`}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+          <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            {children}
+          </main>
         </Providers>
       </body>
     </html>
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { ThemeProviderProps } from "next-themes";
-
 import * as React from "react";
 import { HeroUIProvider } from "@heroui/system";
 import { useRouter } from "next/navigation";
@@ -10,14 +9,6 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 export interface ProvidersProps {
   children: React.ReactNode;
   themeProps?: ThemeProviderProps;
-}
-
-declare module "@react-types/shared" {
-  interface RouterConfig {
-    routerOptions: NonNullable<
-      Parameters<ReturnType<typeof useRouter>["push"]>[1]
-    >;
-  }
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
